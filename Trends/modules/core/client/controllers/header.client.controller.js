ec8b86912d07379dd40151bb86b1c5f0d94425ee
concon52 <conorsullivan@nevada.unr.edu>
2015-12-07 22:34:29 -0800

    Added interactive map.
    Put SVG into images folder in a map folder. code for interactivity is
    done in home client view. Only did hawaii. color for ak is set in SVG
    which is in home client view html file.

--- Trends/modules/core/client/controllers/header.client.controller.js
+++ Trends/modules/core/client/controllers/header.client.controller.js
@@ -2,6 +2,26 @@
 
 angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
   function ($scope, $state, Authentication, Menus) {
+    
+    // // Set up our canvas on the page before doing anything.
+    // var myCanvas = document.createElement('canvas');
+    // myCanvas.width = 1500;
+    // myCanvas.height = 593;
+    // document.getElementsByTagName('body') [0].appendChild(myCanvas);
+      
+    // // Get drawing context for the Canvas
+    // var myCanvasContext = myCanvas.getContext('2d');
+      
+    // // Load up our image.
+    // var source = new Image();
+    // source.src = 'https://upload.wikimedia.org/wikipedia/commons/3/32/Blank_US_Map.svg';
+    // source.width = '100%';
+    // source.height = '100%';
+    // // Render our SVG image to the canvas once it loads.
+    // source.onload = function(){
+    //     myCanvasContext.drawImage(source,170,0);
+    // }
+
     // Expose view variables
     $scope.$state = $state;
     $scope.authentication = Authentication;
