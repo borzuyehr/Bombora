ff0332bd56f014b718d9710cae8ddd9a77cd5995
concon52 <conorsullivan@nevada.unr.edu>
2016-04-25 18:49:27 -0700

    fixed hover stick
    hover effect used to stick on last active element.

--- Trends/modules/core/client/controllers/home.client.controller.js
+++ Trends/modules/core/client/controllers/home.client.controller.js
@@ -56,9 +56,13 @@ angular.module('core').directive('region', ['$compile', function ($compile) {
         scope.hoverRegion = scope.elementId;
         element[0].parentNode.appendChild(element[0]);
       };
+      scope.regionMouseLeave = function() {
+        scope.hoverRegion = 0;
+      };
       element.attr('ng-click', 'regionClick()');
       element.attr('ng-attr-fill', '{{dummyData[elementId].value/100 | map_colour}}');
       element.attr('ng-mouseover', 'regionMouseOver()');
+      element.attr('ng-mouseleave', 'regionMouseLeave()');
       element.attr('ng-class', '{active:hoverRegion==elementId}');
       element.removeAttr('region');
       $compile(element)(scope);
