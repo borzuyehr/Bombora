42e69a99961d853bdf7bc5453282ea24bfef9c5a
bradleywalsh <bradleywalsh3@gmail.com>
2016-04-25 18:17:26 -0700

    updates
    updates

--- Trends/modules/core/client/controllers/home.client.controller.js
+++ Trends/modules/core/client/controllers/home.client.controller.js
@@ -76,11 +76,18 @@ angular.module('core').filter('map_colour', [function () {
 
 angular.module('core').controller('GraphController',
   function ($scope) {
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A', 'Series B'];
+    $scope.labels = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+                     'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD',
+                     'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH',
+                     'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+                     'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
+    $scope.series = ['Series A'];
     $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
+      [65, 59, 80, 81, 56, 55, 40, 33, 90, 14,
+       15, 29, 34, 73, 53, 55, 40, 33, 90, 14,
+       65, 59, 80, 81, 56, 55, 40, 33, 90, 14,
+       65, 59, 80, 81, 56, 55, 40, 33, 90, 14,
+       65, 59, 80, 81, 56, 55, 40, 33, 90, 14]
     ];
   });
 
