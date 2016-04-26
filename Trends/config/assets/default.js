990de54b2c8dce4e02af70e5c634898409a47056
bradleywalsh <bradleywalsh3@gmail.com>
2016-04-25 17:52:41 -0700

    the chart
    chart is reggie

--- Trends/config/assets/default.js
+++ Trends/config/assets/default.js
@@ -6,7 +6,7 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/nvd3/build/nv.d3.css'
+        'public/lib/angular-chart.js/dist/angular-chart.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -19,8 +19,9 @@ module.exports = {
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/d3/d3.js'
-
+        'public/lib/d3/d3.js',
+        'public/lib/Chart.js/Chart.min.js',
+        'public/lib/angular-chart.js/dist/angular-chart.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
